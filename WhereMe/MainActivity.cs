8be549a8e198edd5a01c1b574ad8ccca095db1cd
mitrajaime <mitrajaime@gmail.com>
2024-04-28 14:38:18 +0800

    Working google maps

--- WhereMe/MainActivity.cs
+++ WhereMe/MainActivity.cs
@@ -1,4 +1,6 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
+using Android.Gms.Maps;
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
@@ -6,15 +8,37 @@ using AndroidX.AppCompat.App;
 namespace WhereMe
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
     {
+        readonly string[] permissionGroup =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation,
+        };
+
+        GoogleMap map;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            RequestPermissions(permissionGroup, 0);
+
+            SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
         }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            //var mapStyle = MapStyleOptions.LoadRawResourceStyle(this, Resource.Raw.mapStyle);
+            //googleMap.SetMapStyle(mapStyle);
+            map = googleMap;
+
+            //map.UiSettings.ZoomControlsEnabled = true;
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
