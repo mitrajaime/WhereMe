0d1ec818828cbf3bf67a6fb826abef5f63a80d99
JAIME-ACT <ACT-STUDENT@LAB6-PC29>
2024-05-03 15:22:54 +0800

    added get current location

--- WhereMe/MainActivity.cs
+++ WhereMe/MainActivity.cs
@@ -1,10 +1,13 @@
 ﻿using Android;
 using Android.App;
+using Android.Gms.Location;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using AndroidX.Core.App;
 
 namespace WhereMe
 {
@@ -16,8 +19,11 @@ namespace WhereMe
             Manifest.Permission.AccessCoarseLocation,
             Manifest.Permission.AccessFineLocation,
         };
-
+        ImageButton locationButton;
         GoogleMap map;
+        IFusedLocationProviderClient flpc;
+        Android.Locations.Location myLastLocation;
+        private LatLng myPosition;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -29,6 +35,18 @@ namespace WhereMe
 
             SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
             mapFragment.GetMapAsync(this);
+
+            locationButton = FindViewById<ImageButton>(Resource.Id.locationButton);
+
+            locationButton.Click += LocationButton_Click;
+        }
+
+        private void LocationButton_Click(object sender, System.EventArgs e)
+        {
+            if (checkPermission())
+            {
+                displayLocationAsync();
+            }
         }
 
         public void OnMapReady(GoogleMap googleMap)
@@ -45,6 +63,48 @@ namespace WhereMe
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if(grantResults.Length < 1)
+            {
+                return;
+            }
+
+            if (grantResults[0] == Android.Content.PM.Permission.Granted)
+            {
+                displayLocationAsync();
+            }
+
+        }
+
+        bool checkPermission()
+        {
+            bool permissionGranted = false;
+            if(ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) != Android.Content.PM.Permission.Granted &&
+                ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != Android.Content.PM.Permission.Granted)
+            {
+                permissionGranted = false;
+            }
+            else
+            {
+                permissionGranted = true;
+            }
+            return permissionGranted;
+        }
+
+        async void displayLocationAsync()
+        {
+            if (flpc == null)
+            {
+                flpc = LocationServices.GetFusedLocationProviderClient(this);
+            }
+
+            myLastLocation = await flpc.GetLastLocationAsync();
+
+            if (myLastLocation != null)
+            {
+                myPosition = new LatLng(myLastLocation.Latitude, myLastLocation.Longitude);
+                map.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(myPosition, 20));
+            }
         }
     }
 }
